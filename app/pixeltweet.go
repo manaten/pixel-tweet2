551332af8d305ce3cf592a8463c27b03281cd28c
manaten <manato_kato@dwango.co.jp>
2014-07-07 18:27:04 +0900

    † ad api. †

--- app/pixeltweet.go
+++ app/pixeltweet.go
@@ -1,24 +1,31 @@
 package pixeltweet
 
 import (
+	"encoding/json"
 	"github.com/go-martini/martini"
 	"html/template"
 	"net/http"
+	"time"
+
+	"appengine"
+	"appengine/datastore"
 )
 
 var cached_templates = template.Must(template.ParseGlob("templates/*.html"))
 
 type Entry struct {
+	Id          int64 `datastore:"-"`
 	Title       string
 	Image       string
 	Description string
+	Date        time.Time
 }
 
 func init() {
 	m := martini.Classic()
 	m.Get("/", index)
 	m.Group("/api/entry", func(r martini.Router) {
-		// r.Get("/:id", getEntry)
+		r.Get("", getEntry)
 		r.Post("", newEntry)
 		// r.Put("/:id", updateEntry)
 		// r.Delete("/:id", deleteEntry)
@@ -27,14 +34,54 @@ func init() {
 	http.Handle("/", m)
 }
 
-func newEntry() string {
-	return "{}"
+func entryKey(c appengine.Context) *datastore.Key {
+	return datastore.NewKey(c, "Entry", "default_entry", 0, nil)
+}
+
+// datastoreからクエリで取得しJSONレスポンスで返すとこまでまとめていいかも
+func getEntry(res http.ResponseWriter, req *http.Request) string {
+	c := appengine.NewContext(req)
+	q := datastore.NewQuery("Entry").Order("Date").Limit(50)
+	entries := make([]Entry, 0, 50)
+	keys, err := q.GetAll(c, &entries)
+	if err != nil {
+		return "ng"
+	}
+
+	for i, key := range keys {
+		entries[i].Id = key.IntID()
+	}
+	res.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json, err := json.Marshal(entries)
+	if err != nil {
+		return "ng"
+	}
+	return string(json)
+}
+
+// 同じく、datastoreに突っ込んでレスポンス返すとこまでまとめていいかも
+func newEntry(res http.ResponseWriter, req *http.Request) string {
+	c := appengine.NewContext(req)
+	e := Entry{
+		0,
+		req.FormValue("title"),
+		req.FormValue("image"),
+		req.FormValue("description"),
+		time.Now(),
+	}
+
+	key := datastore.NewIncompleteKey(c, "Entry", entryKey(c))
+	_, err := datastore.Put(c, key, &e)
+	if err != nil {
+		return "ng"
+	}
+	return "ok"
 }
 
 func index(res http.ResponseWriter, req *http.Request) {
 	e := []Entry{
-		Entry{"hoge", "fuga", "hage"},
-		Entry{"foo", "bar", "baz"},
+		Entry{0, "hoge", "fuga", "hage", time.Now()},
+		Entry{0, "foo", "bar", "baz", time.Now()},
 	}
 	cached_templates.ExecuteTemplate(res, "index.html", e)
 }
